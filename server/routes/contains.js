17c2da4564dff9a1f3dd7adaccc6bb2499727200
Callan Bush <callan@snappr.co>
2018-06-20 19:36:26 -0700

    Changed var to let ;)

--- server/routes/contains.js
+++ server/routes/contains.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res, next) => {
   let result = false;
   const tags = await parseHtml(req.query.endpoint, req.query.tag);
 
-  for(var item of tags[req.query.tag]) {
+  for(let item of tags[req.query.tag]) {
     if(item.innerText.indexOf(decodeURIComponent(req.query.text)) > -1) {
       result = true;
       break;
